6b14325d62a5084395440585a7faf46361fb6c2f
Felipe Adarve <adarvefelipe58@gmail.com>
2025-11-05 09:11:50 -0500

    Estructura inicial

---
+++ src/main/java/com/tetris/tetrisburger_backend/TetrisburgerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tetris.tetrisburger_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrisburgerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetrisburgerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetris/tetrisburger_backend/TetrisburgerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetris.tetrisburger_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisburgerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
