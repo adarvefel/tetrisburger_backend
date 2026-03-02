0ac537de01dccdeeb99f19b724f4913150def571
Felipe Adarve <adarvefelipe58@gmail.com>
2026-03-02 10:52:16 -0500

    arreglos de supplier y product categoria despues de Qa

--- src/main/java/com/tetris/tetrisburger_backend/domain/port/out/SupplierRepository.java
+++ src/main/java/com/tetris/tetrisburger_backend/domain/port/out/SupplierRepository.java
@@ -3,6 +3,7 @@ package com.tetris.tetrisburger_backend.domain.port.out;
 import com.tetris.tetrisburger_backend.domain.common.PageResponse;
 import com.tetris.tetrisburger_backend.domain.common.PaginationRequest;
 import com.tetris.tetrisburger_backend.domain.model.Supplier;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
 import java.util.Optional;
 
@@ -14,6 +15,8 @@ public interface SupplierRepository {
     void deleteById(Integer id);
 
     boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByName(String name);
 
     PageResponse<Supplier> findAll(String q, PaginationRequest page);
 }
